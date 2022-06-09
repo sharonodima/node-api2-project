26d27a48a6f652a48e3da9361ff46b68e4e82080
Sharon Odima <odimasharon@gmail.com>
2022-06-09 16:39:06 -0400

    Sharon Odima Unit4.1Module2 Project initial commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,4 @@
 // implement your posts router here
+const express = require("express");
+const router = express.Router();
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+const server = express();
+server.use(express.json());
+const postsRouter = require("./posts/posts-router")
+
+server.use("./api/posts", postsRouter)
+
+server.use("*", (req, res) =>{
+    res.status(404).json({
+        message: "Not found!"
+    })
+})
+
+
+module.exports = server;
